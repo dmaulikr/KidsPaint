91bbda6d835dbb415ba7c8953a131f656201c500
Frid <s02093@M0000004.local>
2011-12-31 12:25:08 +0100

    Initial Commit

---
+++ KidsPaint/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  KidsPaint
+//
+//  Created by Frid, Jonas on 2011-12-31.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
